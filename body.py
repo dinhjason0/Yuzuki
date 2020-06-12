bfc3c0117339df9153fc3f87e82ec2f8d4303ccc
Jason <dinhjason0@gmail.com>
2020-06-12 02:12:54 -0700

    Learned my lesson about functions heroes.py will be dealt with and elaborated on, its made though Silenced the uptime clock in body.py Made edits to the start command, link start added the csv pend sheet to track playerstatistics.csv, thank god

--- body.py
+++ body.py
@@ -3,28 +3,30 @@ import asyncio
 import discord
 from discord.ext import commands
 import time
+import pandas as pd
 import brokken
+from heroes import Hero
 
 #ID 718221388963381309
 
+
 #This is discord.py's call. commands.Bot(lambada: prefix)
-client = commands.Bot(command_prefix = '.', case_insensitive=True)
+client = commands.Bot(command_prefix = '.', case_insensitive=True, owner_id=154662042513440768)
+
+#Pandas Read Player Stats
+ps = pd.read_csv('playerstatistics.csv')
+print(ps)
 
 #Good Morning, Yuzuki
 @client.event
 async def on_ready():
     print(f'{client.user.name} Is Awake')
-    await client.loop.create_task(change_status())
-
+    client.loop.create_task(change_status())
 
 #Uptime Clock
 async def change_status():
     #The "currently playing" message on Yuzuki
-    i = 0
-    while True:
-        await client.change_presence(activity = discord.Game(f"The bot has been live for {++i} seconds"))
-        await asyncio.sleep(1)
-#Change_presence(activity), check documentation.
+        await client.change_presence(activity=discord.Game(f"This message is pissing me off"))
 
 #Member Join
 @client.event
@@ -42,14 +44,34 @@ async def on_member_remove(member):
 async def ping(dtx):
     await dtx.send(f"Your ping is {round(client.latency * 1000)}ms")
 
-#LINK START
-#If user has 'player' role, disable them from having permission to use this command again
+#LINK START: If user has 'player' role, disable them from having permission to use this command again
 @client.command(name = 'start')
-async def start(ctx, guild: discord.Guild = None):
-    guild = ctx.guild if not guild else guild
-    role = ('Player')
+async def start(ctx):
+    role = discord.utils.get(ctx.guild.roles, name='Player')
     if role not in ctx.author.roles:
         await ctx.author.send("Thank you for starting. Lets begin.")
         await ctx.send(f"Link connection has been established. Say hello to your room...")
+        await ctx.author.add_roles(role)
+        pend(ctx.author.id, ctx.author.display_name)
+    pass
+
+def pend(id, display_name):
+    hero = Hero(id, display_name)
+    basis = {
+        'userID': [hero.userID],
+        'name': [hero.name],
+        'HP': [hero.HP],
+        'STR': [hero.STR],
+        'DEX': [hero.DEX],
+        'SPD': [hero.SPD],
+        'MPE': [hero.MPE],
+        'HIT': [hero.HIT]
+    }
+    columns = ['userID','name','HP','STR','DEX','SPD','MPE','HIT']
+    heropend = pd.DataFrame(basis, columns=columns)
+    print(f'\n\nNEW PLAYER: {hero.name}\n{heropend}')
+    global ps
+    ps = ps.append(heropend, ignore_index=True)
+    ps.to_csv('playerstatistics.csv', index = False)
 
 client.run(brokken.token)
--- heroes.py
+++ heroes.py
@@ -1,1 +1,18 @@
 class Hero:
+    def __init__(self, userID='', name='', HP=10, STR = 1, DEX = 1, SPD = 1, MPE = 1, HIT = 5):
+        #User's Discord ID
+        self.userID = userID
+        #Discord#0000 set
+        self.name = name
+        #Player's Total Flat Hitpoints
+        self.HP = HP
+        #Player's Total Flat Strength Stat
+        self.STR = STR
+        #Player's Total Flat Dexterity Stat
+        self.DEX = DEX
+        #Player's Total Flat Speed Stat
+        self.SPD = SPD
+        #Player's Total Flat 'Magic Power Effectiveness'
+        self.MPE = MPE
+        #Player's Hit Percentage
+        self.HIT = HIT
